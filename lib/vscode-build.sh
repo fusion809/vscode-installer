ca78e9aa9141f8a67734ed6ec12ad9ee6968bc7f
fusion809 <brentonhorne77@gmail.com>
2016-05-12 08:53:49 +1000

    updating

---
+++ lib/vscode-build.sh
@@ -0,0 +1,67 @@
+function vscode-build {
+  export GHUB=$HOME/GitHub
+
+  # Get the source code
+  printf "How would you like to get the source code? [curl/git/wget/?] "
+  read SRC_METHOD
+
+  printf "Where do you want to store the source code? [Leavy empty for $GHUB] "
+  read SRC_DEST
+
+  if ! [[ -n $SRC_DEST ]]; then
+    SRC_DEST=$GHUB
+  fi
+
+  printf "Do you want to install VScode locally or system-wide? [local/system] "
+  read DEST_TYPE
+
+  ver=$(sed -n 's/ver=//p' ./lib/version.sh)
+
+  if [[ $SRC_METHOD == "?" ]]; then
+
+    printf "curl and wget are the fastest methods and they chew up less bandwidth.\n
+    While git uses up more bandwidth but it also makes upgrading the package faster."
+
+  elif [[ $SRC_METHOD == "curl" ]]; then
+
+    if [[ -d $SRC_DEST/vscode-$ver ]]; then
+      rm -rf $SRC_DEST/vscode-$ver
+    fi
+    curl -sL https://github.com/Microsoft/vscode/archive/$ver.tar.gz | tar xz -C $SRC_DEST
+    cd $SRC_DEST/vscode-$ver
+
+  elif [[ $SRC_METHOD == "wget" ]]; then
+
+    if [[ -d $SRC_DEST/vscode-$ver ]]; then
+      rm -rf $SRC_DEST/vscode-$ver
+    fi
+    wget -cqO- https://github.com/Microsoft/vscode/archive/$ver.tar.gz | tar xz -C $SRC_DEST
+    cd $SRC_DEST/vscode-$ver
+
+  elif [[ $SRC_METHOD == "git" ]]; then
+    if ! [[ -d $SRC_DEST/vscode ]]; then
+      git clone https://github.com/Microsoft/vscode $SRC_DEST/vscode
+    fi
+    cd $SRC_DEST/vscode
+    git fetch -p
+    git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+
+  else
+
+    printf "You must select a SRC_METHOD!"
+
+  fi
+
+  ARCH=$(uname -m)
+  if [[ $ARCH == 'x86_64' ]]; then
+    _vscode_arch=x64
+  else
+    _vscode_arch=x86
+  fi
+
+  # The build
+  scripts/npm.sh install
+  node --max_old_space_size=2000 /usr/bin/gulp vscode-linux-${_vscode_arch}
+}
+
+export -f vscode-build

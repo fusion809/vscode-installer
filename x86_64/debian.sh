3a069e72734e36f5648e32ac68f60e511a191142
fusion809 <brentonhorne77@gmail.com>
2016-05-12 10:01:35 +1000

    Updating

---
+++ x86_64/debian.sh
@@ -0,0 +1,14 @@
+printf "Do you want to install Visual Studio Code (VScode) from A) a binary package (faster) or would you prefer B) a source install (slower)? [A/B] "
+read preference
+
+if [[ $preference == "A" ]]; then
+
+  curl -sL "https://go.microsoft.com/fwlink/?LinkID=760868" > vscode-amd64.deb
+  sudo dpkg -i vscode-amd64.deb
+  sudo apt-get -f install
+
+elif [[ $preferenc == "B" ]]; then
+
+  debian-build
+
+fi

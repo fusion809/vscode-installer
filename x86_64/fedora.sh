cdcadce13701bbb9bc2aefd554926387ff4dc23a
fusion809 <brentonhorne77@gmail.com>
2016-05-12 14:24:23 +1000

    Updating

--- x86_64/fedora.sh
+++ x86_64/fedora.sh
@@ -1,8 +1,6 @@
 printf "Do you want to install Visual Studio Code (VScode) from A) a binary package (faster) or would you prefer B) a source install (slower)? [A/B] "
 read preference
 
-. ./lib/fedora-build.sh
-
 if [[ $preference == "A" ]]; then
 
   curl -sL "https://go.microsoft.com/fwlink/?LinkID=760667" > vscode-x86_64.rpm

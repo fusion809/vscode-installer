24cdf87c37eb13ad67dacd484ab26ba6dfba3620
fusion809 <brentonhorne77@gmail.com>
2016-05-12 13:27:01 +1000

    Updating

--- lib/centos-build.sh
+++ lib/centos-build.sh
@@ -1,10 +1,12 @@
 . ./lib/vscode-build.sh
+. ./lib/node-build.sh
 
 function centos-build {
   # Get dependencies
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -
-  sudo yum install -y python nodejs-devel gcc gcc-c++ make \
-    glibc-devel git-core libgnome-keyring-devel libX11-devel
+  sudo yum install -y gcc gcc-c++ make glibc-devel \
+    git-core libgnome-keyring-devel libX11-devel
+  sudo yum install -y nodejs-devel || node-build
   sudo npm install -g gulp
   vscode-build
 }

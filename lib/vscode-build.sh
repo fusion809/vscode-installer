1bf6a76437da5ee04750f165d9f288408378a83f
fusion809 <brentonhorne77@gmail.com>
2016-05-12 12:49:35 +1000

    Updating

--- lib/vscode-build.sh
+++ lib/vscode-build.sh
@@ -68,7 +68,7 @@ function vscode-build {
     cd ..
     sudo mv VSCode-linux-${_vscode_arch} /opt
     sudo ln -s "/opt/VSCode-linux-${_vscode_arch}/code-oss" "/usr/bin/visual-studio-code-oss"
-    sed -i -e "s/VSCode-OSS/VSCode-linux-${_vscode_arch}/g" "$GHUB/VScode-installer/visual-studio-code-oss.desktop"
+    sed -i -e "s|<%-INST-%>|/opt/VSCode-linux-${_vscode_arch}|g" "$GHUB/VScode-installer/visual-studio-code-oss.desktop"
     sudo install -D -m644 "$GHUB/VScode-installer/visual-studio-code-oss.desktop" \
             "/usr/share/applications/visual-studio-code-oss.desktop" || sudo install -D -m644 "$GHUBM/VScode-installer/visual-studio-code-oss.desktop" \
                         "/usr/share/applications/visual-studio-code-oss.desktop"

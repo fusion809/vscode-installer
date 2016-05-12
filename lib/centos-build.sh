65c21a331f21ce33c2803071615b0ea21c345911
fusion809 <brentonhorne77@gmail.com>
2016-05-12 13:24:51 +1000

    Updating

--- lib/centos-build.sh
+++ lib/centos-build.sh
@@ -2,7 +2,7 @@
 
 function centos-build {
   # Get dependencies
-  curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -     # Get the latest version of Node.js
+  curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -
   sudo yum install -y python nodejs-devel gcc gcc-c++ make \
     glibc-devel git-core libgnome-keyring-devel libX11-devel
   sudo npm install -g gulp

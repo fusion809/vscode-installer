5a043c369f612ff7068683f881f895919d870c12
fusion809 <brentonhorne77@gmail.com>
2016-05-12 14:53:06 +1000

    Updating

--- installer.sh
+++ installer.sh
@@ -39,6 +39,8 @@ elif [[ $LD == "Mageia"* ]]; then
   source "lib/mageia-build.sh"
 elif [[ $LD == "openSUSE"* ]]; then
   source "lib/opensuse-build.sh"
+elif [[ $LD == "Sabayon"* ]]; then
+  source "lib/sabayon-build.sh"
 elif [[ $LD == "Ubuntu"* ]]; then
   source "lib/ubuntu-build.sh"
 fi
@@ -53,6 +55,7 @@ DISTROS=(
 'Mageia'
 'Manjaro'                                   #  Manjaro Linux
 'openSUSE'                                  #  openSUSE
+'Sabayon'
 'Ubuntu'                                    #  Ubuntu
 )
 

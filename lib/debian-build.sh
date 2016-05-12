77c7034f5535978891d13ea9276ca2af00663f7c
fusion809 <brentonhorne77@gmail.com>
2016-05-12 11:08:50 +1000

    Updating

---
+++ lib/debian-build.sh
@@ -0,0 +1,12 @@
+. ./lib/vscode-build.sh
+
+function debian-build {
+  # Get dependencies
+  sudo apt-get install -y curl
+  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  sudo apt-get install -y nodejs build-essential git \
+    libgnome-keyring-dev fakeroot
+  vscode-build
+}
+
+export -f vscode-build

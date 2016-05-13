37207bbadd54a5281a29d246a527677a6eefe86e
fusion809 <brentonhorne77@gmail.com>
2016-05-13 19:15:32 +1000

    Updating

--- installer.sh
+++ installer.sh
@@ -16,7 +16,7 @@ fi
 # substitute was made.
 export LD=$(cat /etc/os-release | grep -w "NAME" | sed 's/NAME=//g' | sed 's/"//g')
 # Distribution architecture
-export ARCH=$(uname -m)
+export ARCH=$(uname -m | sed 's/3/6/g' | sed 's/4/6/g' | sed 's/5/6/g')
 # Distribution version number, e.g., on Fedora 23 it returns 23
 export VER=$(cat /etc/os-release | grep -w "VERSION_ID" | sed 's/VERSION_ID=//g' | sed 's/"//g')
 

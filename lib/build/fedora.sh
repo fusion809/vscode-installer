d2f8c2fb739e11dc0aca615e18bf36a932d3af2c
fusion809 <brentonhorne77@gmail.com>
2016-05-15 19:08:50 +1000

    Updating

--- lib/build/fedora.sh
+++ lib/build/fedora.sh
@@ -1,11 +1,13 @@
 . ./lib/build/vscode.sh
+. ./lib/build/node.sh
 
 function fedora-build {
   # Get dependencies
-  sudo dnf copr enable nibbler/nodejs || sudo dnf install -y 'dnf-command(copr)' \
-    && sudo dnf copr enable -y nibbler/nodejs
   sudo dnf update
-  sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel tar nodejs npm libX11-devel
+  sudo dnf install -y make gcc gcc-c++ glibc-devel git-core libgnome-keyring-devel tar npm libX11-devel
+  sudo dnf remove -y nodejs
+
+  node-build
 
   if ! `comex gulp`; then
     sudo npm install -g gulp
@@ -18,7 +20,7 @@ function fedora-build {
   if ! [[ -f /usr/bin/gyp ]]; then
     sudo ln -s /usr/lib/node_modules/node-gyp /usr/bin/gyp
   fi
-  
+
   vscode-build
 }
 

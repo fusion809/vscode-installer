499c370838ce37f25419c9032f1d5ef5a9de9c0e
fusion809 <brentonhorne77@gmail.com>
2016-05-16 23:37:35 +1000

    Updating

--- lib/src-build.sh
+++ lib/src-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function src-build {
   # Install NPM dependencies
-  scripts/npm.sh install || npm install oniguruma@6.0.1 && scripts/npm.sh install
+  /bin/bash -c "scripts/npm.sh install"
 
   # Build vscode
   node --max_old_space_size=2000 /usr/bin/gulp vscode-linux-${_vscode_arch} || printf "An error has occurred while building this package with gulp. Please report the exact error message you received\n at https://github.com/fusion809/VScode-installer/issues/new"

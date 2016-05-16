bfc7642421c5e44e0808eb060a0a69b7e3d6257b
fusion809 <brentonhorne77@gmail.com>
2016-05-16 23:05:40 +1000

    Updating

--- lib/method.sh
+++ lib/method.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function method {
-  printf "Would you rather install Visual Studio Code from: A) a Debian binary (faster) or B) from source (slower, but open-source)? [A/B] "
+  printf "Would you rather install Visual Studio Code from: A) a binary package (faster) or B) from source (slower, but open-source)? [A/B] "
   read method
 }
 

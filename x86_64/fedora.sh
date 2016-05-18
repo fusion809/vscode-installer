93d71bf4a0c26f55ae83c5025870dd64e932659b
fusion809 <brentonhorne77@gmail.com>
2016-05-18 22:01:08 +1000

    fixing error

--- x86_64/fedora.sh
+++ x86_64/fedora.sh
@@ -35,8 +35,8 @@ fi
 
 if [[ $method == "A" ]]; then
 
-  curl -sL "https://go.microsoft.com/fwlink/?LinkID=760667" > vscode-x86_64.rpm
-  sudo dnf install -y vscode-x86_64.rpm
+  curl -sL "https://go.microsoft.com/fwlink/?LinkID=760667" > /tmp/vscode-x86_64.rpm
+  sudo dnf install -y /tmp/vscode-x86_64.rpm
 
 elif [[ $method == "B" ]]; then
 

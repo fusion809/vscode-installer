4e96bebe5c7188bd2efaea46d82743e69284aed7
fusion809 <brentonhorne77@gmail.com>
2016-05-19 02:48:32 +1000

    Improving local install

--- lib/method.sh
+++ lib/method.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function method {
-  printf "Would you rather install Visual Studio Code from: A) a binary package (faster and safer) or B) from source (slower, but open-source)? \n[Available options: A/B. If you enter input other than A/B then a source install will be performed] "
+  printf "Would you rather install Visual Studio Code from: A) a binary package (faster and safer) or B) from source (slower, but open-source)? \n[Available options: A/B. If you enter input other than A or B then a source install will be performed]\n"
   read method
 
   if ! [[ -n $method ]]; then

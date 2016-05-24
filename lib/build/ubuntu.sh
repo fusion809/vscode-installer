949f16fec060380b1a4e6f561577ba7566625596
fusion809 <brentonhorne77@gmail.com>
2016-05-24 19:22:47 +1000

    Updating

--- lib/build/ubuntu.sh
+++ lib/build/ubuntu.sh
@@ -2,10 +2,18 @@
 
 function ubuntu_build {
   # Get dependencies
-  sudo apt-get install -y curl
-  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-  sudo apt-get install -y nodejs build-essential git \
+  sudo apt-get install -y curl build-essential git \
     libgnome-keyring-dev fakeroot libx11-dev python
+  curl -L "https://projects.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/nodejs" > /tmp/PKGBUILD
+  nodelver=$(sed -n 's/pkgver=//p' /tmp/PKGBUILD)
+  nodever=$(node --version | sed 's/v//g')
+  if ! [[ $nodever == $nodelver ]]; then
+    if curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -; then
+      sudo apt-get install -y nodejs
+    else
+      node_build
+    fi
+  fi
 
   if ! `comex gulp`; then
     sudo npm install -g gulp

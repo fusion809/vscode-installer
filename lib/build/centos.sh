6e678d9381f4b460f5f62acbc965a2c36a2eaa25
fusion809 <brentonhorne77@gmail.com>
2016-05-25 05:35:14 +1000

    Updating

--- lib/build/centos.sh
+++ lib/build/centos.sh
@@ -5,7 +5,7 @@ function centos_build {
   # Get dependencies
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -
   sudo yum install -y gcc gcc-c++ make glibc-devel git-core python \
-    libgnome-keyring-devel libX11-devel createrepo rpmdevtools
+    libgnome-keyring-devel libX11-devel createrepo rpmdevtools fakeroot
   sudo yum install -y nodejs-devel || node_build
 
   if ! `comex gulp`; then

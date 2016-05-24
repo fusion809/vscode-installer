cfbe1e1a1bf09c0c3a430b2385406318c04e18f9
fusion809 <brentonhorne77@gmail.com>
2016-05-25 01:18:26 +1000

    [TE] Updating

--- lib/build/opensuse.sh
+++ lib/build/opensuse.sh
@@ -4,7 +4,7 @@
 function opensuse_build {
   # Get dependencies
   sudo zypper in -y nodejs nodejs-devel make gcc gcc-c++ glibc-devel \
-    git-core libgnome-keyring-devel libX11-devel python
+    git-core libgnome-keyring-devel libX11-devel python customrepo
 
   if ! `comex gulp`; then
     sudo npm install -g gulp
@@ -12,6 +12,10 @@ function opensuse_build {
 
   if ! `comex node-gyp`; then
     sudo npm install -g node-gyp
+  fi
+
+  if ! [[ -f /usr/bin/gyp ]]; then
+    sudo ln -sf /usr/lib/node_modules/node-gyp /usr/bin/gyp
   fi
 
   vscode_build

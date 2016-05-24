b5c8d3da7c641d10bec2a1070122096d371093b5
fusion809 <brentonhorne77@gmail.com>
2016-05-25 05:40:57 +1000

    Updating

--- lib/build/opensuse.sh
+++ lib/build/opensuse.sh
@@ -4,7 +4,16 @@
 function opensuse_build {
   # Get dependencies
   sudo zypper in -y nodejs nodejs-devel make gcc gcc-c++ glibc-devel \
-    git-core libgnome-keyring-devel libX11-devel python createrepo rpm-build fakeroot
+    git-core libgnome-keyring-devel libX11-devel python createrepo rpm-build
+
+  if [[ $LD == "openSUSE Leap" ]]; then
+    sudo zypper addrepo http://download.opensuse.org/repositories/devel:tools/openSUSE_Leap_42.1/devel:tools.repo
+  elif [[ $LD == "openSUSE Tumbleweed" ]]; then
+    sudo zypper addrepo http://download.opensuse.org/repositories/devel:tools/openSUSE_Tumbleweed/devel:tools.repo
+  fi
+  
+  sudo zypper refresh
+  sudo zypper in -y fakeroot
 
   if ! `comex gulp`; then
     sudo npm install -g gulp

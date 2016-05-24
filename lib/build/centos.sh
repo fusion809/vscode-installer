a8386810cb3a24b8238493028092666627bf0635
fusion809 <brentonhorne77@gmail.com>
2016-05-25 01:18:06 +1000

    [TE] Updating

--- lib/build/centos.sh
+++ lib/build/centos.sh
@@ -5,7 +5,7 @@ function centos_build {
   # Get dependencies
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -
   sudo yum install -y gcc gcc-c++ make glibc-devel \
-    git-core libgnome-keyring-devel libX11-devel python
+    git-core libgnome-keyring-devel libX11-devel python customrepo
   sudo yum install -y nodejs-devel || node_build
 
   if ! `comex gulp`; then
@@ -14,6 +14,10 @@ function centos_build {
 
   if ! `comex node-gyp`; then
     sudo npm install -g node-gyp
+  fi
+
+  if ! [[ -f /usr/bin/gyp ]]; then
+    sudo ln -sf /usr/lib/node_modules/node-gyp /usr/bin/gyp
   fi
 
   vscode_build

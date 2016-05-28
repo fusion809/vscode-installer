f420e9a1e35e59407fb5d42999bb030e6c15f75c
fusion809 <brentonhorne77@gmail.com>
2016-05-28 17:31:54 +1000

    Updating

--- lib/src_prepare.sh
+++ lib/src_prepare.sh
@@ -2,7 +2,8 @@
 function src_prepare {
   cp $INDIR/resources/code.png $SRC_DEST/${lowedit}-$pkgver/resources/linux/code.png
   cp $INDIR/resources/visual-studio-code-oss.desktop $SRC_DEST/${lowedit}-$pkgver/resources/linux/code.desktop
-  sed -i -e "s|<%-DESC-%>|$DESC|g" "$SRC_DEST/${lowedit}-$pkgver/resources/linux/code.desktop"
+  sed -i -e "s|<%-DESC-%>|$DESC|g" \
+         -e "s|<%=INST=%>|$INST_DEST|g" "$SRC_DEST/${lowedit}-$pkgver/resources/linux/code.desktop"
 }
 
 export -f src_prepare

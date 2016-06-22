ecbe46301a066c3c1f6a34a9be276742cd58eb3f
fusion809 <brentonhorne77@gmail.com>
2016-06-22 13:13:02 +1000

    Updating

--- lib/build/centos.sh
+++ lib/build/centos.sh
@@ -1,11 +1,17 @@
 . ./lib/build/vscode.sh
 . ./lib/build/node.sh
 
+if [[ -n $ARCHC ]]; then
+  export ARCHC=$(uname -m | sed 's/i[0-9]/i3/g')
+fi
+
 function centos_build {
   # Get dependencies
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -
   sudo yum install -y gcc gcc-c++ make glibc-devel git-core python \
-    libgnome-keyring-devel libX11-devel createrepo rpmdevtools
+    libgnome-keyring-devel libX11-devel createrepo rpmdevtools \
+    ftp://195.220.108.108/linux/centos/6.8/os/$ARCHC/Packages/fakeroot-libs-1.12.2-22.2.el6.$ARCHC.rpm \
+    ftp://195.220.108.108/linux/centos/6.8/os/$ARCHC/Packages/fakeroot-1.12.2-22.2.el6.$ARCHC.rpm
   sudo yum install -y nodejs-devel || node_build
 
   sudo npm install -g gulp node-gyp

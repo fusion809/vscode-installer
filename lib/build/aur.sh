1cb3200ad6c181e9c820aface38bbfde77f83fd5
fusion809 <brentonhorne77@gmail.com>
2016-06-30 23:19:52 +1000

    Fixing, in accordance with my recent renaming of my vscode repos

--- lib/build/aur.sh
+++ lib/build/aur.sh
@@ -2,12 +2,12 @@
 
 function vsin {
   if [[ -n "$1" ]]; then
-    cd /tmp/visual-studio-code-oss/visual-studio-code-$1
+    cd /tmp/code-$1
   else
-    cd /tmp/visual-studio-code
+    cd /tmp/code
   fi
   makepkg -si --noconfirm
-  cd .
+  cd ..
 }
 
 export -f vsin
@@ -17,29 +17,29 @@ function vsaur {
   # Export env variables
   export AUR=https://aur.archlinux.org/cgit/aur.git/snapshot/
   export GIT=https://aur.archlinux.org/
-  export PKG=https://github.com/fusion809/visual-studio-code-oss
+  export PKG=https://github.com/fusion809/code-oss
 
   if [[ -n "$1" ]]; then                     # if input provided; allowed input bin, git
 
     if comex git; then                      # Install with git and makepkg otherwise
 
-      git clone $PKG /tmp/visual-studio-code-oss
+      git clone $PKG /tmp/code-oss
       vsin $1
 
     elif comex curl; then                     # Install with curl and makepkg otherwise
 
-      curl -sL $PKG/archive/master.tar.gz | tar xz --transform=s/visual-studio-code-oss-master/visual-studio-code-oss/ -C /tmp
+      curl -sL $PKG/archive/master.tar.gz | tar xz --transform=s/code-oss-master/code-oss/ -C /tmp
       vsin $1
 
     elif comex wget; then                     # Install with wget and makepkg otherwise
 
-      wget -cqO- $PKG/archive/master.tar.gz | tar xz --transform=s/visual-studio-code-oss-master/visual-studio-code-oss/ -C /tmp
+      wget -cqO- $PKG/archive/master.tar.gz | tar xz --transform=s/code-oss-master/code-oss/ -C /tmp
       vsin $1
 
     else                                      # Install curl and install with curl and makepkg otherwise
 
       sudo pacman -S curl --noconfirm
-      curl -sL $PKG/archive/master.tar.gz | tar xz --transform=s/visual-studio-code-oss-master/visual-studio-code-oss/ -C /tmp
+      curl -sL $PKG/archive/master.tar.gz | tar xz --transform=s/code-oss-master/code-oss/ -C /tmp
       vsin $1
 
     fi
@@ -50,23 +50,23 @@ function vsaur {
 
     elif comex git; then                      # Install with git and makepkg otherwise
 
-      git clone $GIT/visual-studio-code.git /tmp/visual-studio-code
+      git clone $GIT/visual-studio-code.git /tmp/code
       vsin
 
     elif comex curl; then                     # Install with curl and makepkg otherwise
 
-      curl -sL $AUR/visual-studio-code.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/visual-studio-code.tar.gz | tar xz -C /tmp --transform="s/visual-studio-code/code/"
       vsin
 
     elif comex wget; then                     # Install with wget and makepkg otherwise
 
-      wget -cqO- $AUR/visual-studio-code.tar.gz | tar xz -C /tmp
+      wget -cqO- $AUR/visual-studio-code.tar.gz | tar xz -C /tmp --transform="s/visual-studio-code/code/"
       vsin
 
     else                                      # Install curl and install with curl and makepkg otherwise
 
       sudo pacman -S curl --noconfirm
-      curl -sL $AUR/visual-studio-code.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/visual-studio-code.tar.gz | tar xz -C /tmp --transform="s/visual-studio-code/code/"
       vsin
 
     fi
